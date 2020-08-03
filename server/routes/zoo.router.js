687ae5cae9496b307d1197d3aa45c3e71adc0054
Kevin Anderson <kmichael419@hotmail.com>
2020-08-03 13:50:30 -0500

    creating saga to get zoo animals and creating GET route on server to go and get the species and class names from the database

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
-    // YOUR CODE HERE
-    
+  // YOUR CODE HERE
+  const queryString = `SELECT species_name, class_name FROM "species"
+  JOIN "class" ON "class".id = "species".class_id`;
+
+  pool
+    .query(queryString)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -9,11 +9,26 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
+import Axios from 'axios';
+// import { put } from '../server/routes/zoo.router';
+import { takeEvery, put } from 'redux-saga/effects';
 
 // Your saga should listen for the action type of `GET_ZOO_ANIMALS`
 function* rootSaga() {
-    // YOUR CODE HERE
+  // YOUR CODE HERE
+  yield takeEvery('GET_ZOO_ANIMALS', getAnimals);
+}
 
+function* getAnimals(action) {
+  try {
+    const response = yield Axios.get('/zoo');
+    yield put({
+      type: 'SET_ZOO_ANIMALS',
+      payload: response.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Create sagaMiddleware
@@ -21,26 +36,30 @@ const sagaMiddleware = createSagaMiddleware();
 
 // Used to store class and number of unique animals in that class
 const zooAnimals = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_ZOO_ANIMALS':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_ZOO_ANIMALS':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        zooAnimals,
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+  combineReducers({
+    zooAnimals,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
-    document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
